42b89a918a08ff5fdf02946f3d41df55889948f4
CiudadAlex <ciudad.alex@gmail.com>
2024-10-11 01:19:46 +0200

    RobotCarServer

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from pi5neo import Pi5Neo
+from actuators.LED import LED
+import time
+from rpi_ws281x import *
 
-print("hello blinky!")
-
-# Initialize the Pi5Neo class with 6 LEDs and an SPI speed of 800kHz
-neo = Pi5Neo('/dev/spidev10.0', 6, 800)
-
-# Fill the strip with a red color
-neo.fill_strip(255, 0, 0)
-neo.update_strip()  # Commit changes to the LEDs
-
-# Set the 5th LED to blue
-neo.set_led_color(4, 0, 0, 255)
-neo.update_strip()
+if __name__ == '__main__':
+    while True:
+        led = LED()
+        try:
+            led.colorWipe(Color(255,0,0))
+            time.sleep(1)
+            led.colorWipe(Color(0,255,0))
+            time.sleep(1)
+            led.colorWipe(Color(0,0,255))
+            time.sleep(1)
+        except:
+            led.colorWipe(Color(0,0,0))
