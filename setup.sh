a3ee2865e8feaadaf7f092eab84499be73757279
CiudadAlex <ciudad.alex@gmail.com>
2024-10-14 00:06:08 +0200

    RobotCarServer

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 python3 -m venv .venv
 source .venv/bin/activate
 
-sudo ./.venv/bin/pip3 install RPi.GPIO
-sudo ./.venv/bin/pip3 install adafruit-pca9685
-sudo -H ./.venv/bin/pip3 install --upgrade luma.oled
-sudo ./.venv/bin/pip3 install rpi_ws281x
-sudo ./.venv/bin/pip3 install mpu6050-raspberrypi
-sudo ./.venv/bin/pip3 install flask
-sudo ./.venv/bin/pip3 install flask_cors
-sudo ./.venv/bin/pip3 install websockets
-sudo ./.venv/bin/pip3 install imutils zmq pybase64 psutil
-sudo ./.venv/bin/pip3 install opencv-python
+sudo ./.venv/bin/pip3 install RPi.GPIO==0.7.1
+sudo ./.venv/bin/pip3 install adafruit-pca9685==1.0.1
+sudo -H ./.venv/bin/pip3 install --upgrade luma.oled==3.13.0
+sudo ./.venv/bin/pip3 install rpi_ws281x==5.0.0
+sudo ./.venv/bin/pip3 install mpu6050-raspberrypi==1.2
+sudo ./.venv/bin/pip3 install flask==3.0.3
+sudo ./.venv/bin/pip3 install flask_cors==5.0.0
+sudo ./.venv/bin/pip3 install websockets==13.1
+sudo ./.venv/bin/pip3 install imutils==0.5.4
+sudo ./.venv/bin/pip3 install zmq==0.0.0
+sudo ./.venv/bin/pip3 install pybase64==1.4.0
+sudo ./.venv/bin/pip3 install psutil==6.0.0
+sudo ./.venv/bin/pip3 install opencv-python==4.10.0.84
+sudo ./.venv/bin/pip3 install picamera==1.13
 
