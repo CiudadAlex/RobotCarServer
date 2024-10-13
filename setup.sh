0b91259d9dc8ad97bded05667a935618cdd0e136
CiudadAlex <ciudad.alex@gmail.com>
2024-10-14 01:37:34 +0200

    RobotCarServer

--- setup.sh
+++ setup.sh
@@ -25,5 +25,7 @@ sudo ./.venv/bin/pip3 install picamera==1.13
 
 sudo apt install -y python3-libcamera python3-kms++
 sudo apt install -y python3-prctl libatlas-base-dev ffmpeg libopenjp2-7 python3-pip
+sudo apt install -y python3-picamera2
 sudo ./.venv/bin/pip3 install numpy --upgrade
+sudo ./.venv/bin/pip3 install libpcap==1.11.0b12
 sudo ./.venv/bin/pip3 install picamera2
