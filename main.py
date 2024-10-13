6fb767cda4787e5763c9287ad9adb8028c9f595e
CiudadAlex <ciudad.alex@gmail.com>
2024-10-14 00:31:16 +0200

    RobotCarServer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from commanders.KeyboardCommander import KeyboardCommander
-from camera_pi import Camera
+from time import sleep
+from picamera import PiCamera
 
 
 if __name__ == '__main__':
@@ -7,12 +8,12 @@ if __name__ == '__main__':
     # keyboardCommander = KeyboardCommander()
     # keyboardCommander.start()
 
-    camera = Camera()
-    frame = camera.get_frame()
-
-    with open("image.jpg", "wb") as binary_file:
-        # Write bytes to file
-        binary_file.write(frame)
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(2)
+    camera.capture('foo.jpg')
 
 
 # FIXME Camera
