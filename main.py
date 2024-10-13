eb6d754e8decc661b676c2edafb56077843109e4
CiudadAlex <ciudad.alex@gmail.com>
2024-10-13 23:41:23 +0200

    RobotCarServer

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from commanders.KeyboardCommander import KeyboardCommander
+from camera_pi import Camera
 
 
 if __name__ == '__main__':
 
-    keyboardCommander = KeyboardCommander()
-    keyboardCommander.start()
+    # keyboardCommander = KeyboardCommander()
+    # keyboardCommander.start()
+
+    camera = Camera()
+    frame = camera.get_frame()
+
+    with open("image.jpg", "wb") as binary_file:
+        # Write bytes to file
+        binary_file.write(frame)
 
 
 # FIXME Camera
