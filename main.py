cf5a1cf1aecfc12e18486555eb1f67824b6789a1
CiudadAlex <ciudad.alex@gmail.com>
2024-10-15 13:20:38 +0200

    RobotCarServer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # from commanders.KeyboardCommander import KeyboardCommander
 
-# from picamera2 import Picamera2
-from time import sleep
-from picamera import PiCamera
+from picamera2 import Picamera2
+# from time import sleep
+# from picamera import PiCamera
 
 
 if __name__ == '__main__':
@@ -10,16 +10,16 @@ if __name__ == '__main__':
     # keyboardCommander = KeyboardCommander()
     # keyboardCommander.start()
 
-    # picam2 = Picamera2()
-    # picam2.start_and_capture_file("test.jpg")
+    picam2 = Picamera2()
+    picam2.start_and_capture_file("test.jpg")
 
+'''
     camera = PiCamera()
     camera.resolution = (1024, 768)
     camera.start_preview()
-    # Camera warm-up time
     sleep(2)
     camera.capture('foo.jpg')
-
+'''
 
 # FIXME Camera
 # FIXME Microphone
