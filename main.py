8eba3afbdfe826b7aa8cd784d24afcdb500f11fe
CiudadAlex <ciudad.alex@gmail.com>
2024-10-27 15:51:46 +0100

    RobotCarServer

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ speech_2_text_processor.start()
 
 # import CommandServer
 
+# FIXME local speech detection
 # FIXME test Microphone server
 # FIXME test server LED commands
 
--- tools/SpeechDetector.py
+++ tools/SpeechDetector.py
@@ -1,7 +1,4 @@
 import webrtcvad
-from pydub import AudioSegment
-import numpy as np
-import io
 
 
 class SpeechDetector:
@@ -10,25 +7,22 @@ class SpeechDetector:
         self.vad = webrtcvad.Vad(aggressiveness)
 
     @staticmethod
-    def convert_to_pcm(audio_chunk):
-        audio = AudioSegment.from_file(io.BytesIO(audio_chunk))
-        audio = audio.set_channels(1)  # Mono
-        audio = audio.set_frame_rate(16000)  # 16 kHz
-        return np.array(audio.get_array_of_samples())
+    def divide_bytearray(byte_array, piece_size):
+        return [byte_array[i:i + piece_size] for i in range(0, len(byte_array), piece_size)]
 
     def detect_voice(self, audio_chunk):
-
-        audio_pcm = self.convert_to_pcm(audio_chunk)
 
         # Divide in segments of 30 ms (mandatory for VAD)
         frame_duration = 30  # ms
         sample_rate = 16000
         samples_per_frame = int(sample_rate * frame_duration / 1000)
 
+        list_frames = self.divide_bytearray(audio_chunk, samples_per_frame)
+
         # Evaluate each frame to detect speech
         is_speech_detected = False
-        for i in range(0, len(audio_pcm), samples_per_frame):
-            frame = audio_pcm[i:i + samples_per_frame].tobytes()
+        for frame in list_frames:
+
             if self.vad.is_speech(frame, sample_rate):
                 is_speech_detected = True
                 break
