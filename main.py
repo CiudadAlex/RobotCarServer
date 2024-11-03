f45f233420e1acfc9fbee8d14372088405ee7f88
CiudadAlex <ciudad.alex@gmail.com>
2024-11-04 00:35:01 +0100

    RobotCarServer

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ if __name__ == '__main__':
     image_stream_server = ImageStreamServer()
     image_stream_server.start()
 
-    print("######## Start KeyboardCommander")
-    keyboard_commander = KeyboardCommander()
-    keyboard_commander.start()
-
     print("######## Start AudioStreamServer")
     audio_stream_server = AudioStreamServer()
     audio_stream_server.start()
+
+    print("######## Start KeyboardCommander")
+    keyboard_commander = KeyboardCommander()
+    keyboard_commander.start()
 
