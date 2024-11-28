6e50d3622b1d927c004f0ae753bda56844a6a5e0
CiudadAlex <ciudad.alex@gmail.com>
2024-11-29 00:03:14 +0100

    RobotCarServer

--- main.py
+++ main.py
@@ -33,16 +33,14 @@ import time
 
 from actuators.Motor import Motor
 motor = Motor()
-motor.forward(100)
+# motor.forward(100)
 
-time.sleep(20)
-motor.stop()
-
-
+motor.motor_side_left(run=True, direction=0, speed=100)
+motor.motor_side_right(run=True, direction=0, speed=100)
 
 time.sleep(10)
+motor.stop()
 
 print("END______________________")
-
 
 
