0c490debc10cee27f72ff974ec7c736e4740a591
CiudadAlex <ciudad.alex@gmail.com>
2024-11-29 13:53:23 +0100

    RobotCarServer

--- main.py
+++ main.py
@@ -29,9 +29,44 @@ camera_tilt = CameraTilt()
 camera_tilt.home()
 '''
 
+import sys
 import time
-
 from actuators.Motor import Motor
+
+print(sys.argv)
+
+'''
+    Motor_A_EN = 4
+    Motor_A_Pin1 = 26
+    Motor_A_Pin2 = 21
+    
+    Motor_B_EN = 17
+    Motor_B_Pin1 = 27
+    Motor_B_Pin2 = 18
+    
+    -------------------------------
+    
+    Motor_A_EN = 4
+    Motor_A_Pin1 = 14
+    Motor_A_Pin2 = 15
+    
+    Motor_B_EN = 17
+    Motor_B_Pin1 = 27
+    Motor_B_Pin2 = 18
+
+'''
+
+if len(sys.argv) > 1:
+
+    Motor.Motor_A_EN = sys.argv[1]
+    Motor.Motor_A_Pin1 = sys.argv[2]
+    Motor.Motor_A_Pin2 = sys.argv[3]
+
+    Motor.Motor_B_EN = sys.argv[4]
+    Motor.Motor_B_Pin1 = sys.argv[5]
+    Motor.Motor_B_Pin2 = sys.argv[6]
+
+
 motor = Motor()
 # motor.forward(100)
 
