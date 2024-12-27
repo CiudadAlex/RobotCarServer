881079d272d3d4dd0a8ee1b07f04cdc0755ae1d8
CiudadAlex <ciudad.alex@gmail.com>
2024-12-27 17:17:10 +0100

    RobotCarServer

--- background/EmergencyBrake.py
+++ background/EmergencyBrake.py
@@ -5,15 +5,20 @@ from threading import Thread
 
 class EmergencyBrake(Thread):
 
-    def __init__(self):
+    def __init__(self, debug=False):
         super().__init__()
         self.active = True
+        self.debug = debug
 
     def run(self):
 
         while self.active:
 
             distance = ObstacleDetector.check_distance()
+
+            if self.debug:
+                print(f"distance = {distance}")
+
             if distance < 0.15:
                 Commander.execute_move_stop()
                 print("BRAKE!!!!!")
