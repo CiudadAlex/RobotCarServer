2217da56ccaa655dfdf548abdafb9aa1aa79052f
CiudadAlex <ciudad.alex@gmail.com>
2025-03-15 01:13:04 +0100

    RobotCarServer

--- complexcommands/WallFollower.py
+++ complexcommands/WallFollower.py
@@ -2,11 +2,14 @@ from commanders.Commander import Commander
 from tools.DataStorage import DataStorage
 from sensors.ObstacleDetector import ObstacleDetector
 import time
+import threading
 
 
 class WallFollower:
 
-    AWAY_METERS = 0.3
+    CLOSE_METERS = 0.3
+    FAR_AWAY_METERS = 1.0
+
     FORWARD_SECS = 0.7
     TURN_SECS = 0.12
 
@@ -20,11 +23,16 @@ class WallFollower:
 
     def __init__(self):
         self.running = False
+        self.in_contact = False
 
     def stop(self):
         self.running = False
 
     def execute(self):
+        thread = threading.Thread(target=self.execute_inner)
+        thread.start()
+
+    def execute_inner(self):
 
         self.running = True
 
@@ -42,7 +50,14 @@ class WallFollower:
 
     def take_action(self, distance):
 
-        if distance > WallFollower.AWAY_METERS:
+        if distance < WallFollower.CLOSE_METERS:
+            self.in_contact = True
+
+        if not self.in_contact:
+            Commander.execute_move_a_bit_forward(WallFollower.FORWARD_SECS)
+            return
+
+        if distance > WallFollower.CLOSE_METERS:
             Commander.execute_move_a_bit_forward(WallFollower.FORWARD_SECS)
             return
 
